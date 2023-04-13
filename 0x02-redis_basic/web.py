7a6e4279e7c1e26aa0f508519dfe8fe103ef5da9
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-13 06:01:19 +0100

    Web expire

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Implement a get_page
+
+"""
+import redis
+import requests
+
+
+def get_page(url: str) -> str:
+
+    redie = redis.Redis()
+    def wrapper():
+        redie.incr("count:{}".format(url))
+        redie.expire("count:{}".format(url), 10)
+
+        page = requests.get(url)
+        return page.text
+
+    return wrapper
