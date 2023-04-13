fc4c39fa225172fd46af09abeed65fce7119d9ac
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-13 04:01:52 +0100

    Implement get functions that get the stored data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -28,3 +28,45 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: typing.Callable) -> typing.Union[
+                                                                str,
+                                                                bytes,
+                                                                int,
+                                                                float]:
+        """Gets a data stored at a key.
+
+        Args:
+            key (str): Key used to store the data.
+            fn (typing.Callable): Callback function to render
+                                the returned data in the right format.
+
+        Returns:
+            typing.Union[str, bytes, int, float]: Stored data.
+        """
+        data: bytes = self._redis.get(key)
+        if fn:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Gets data stored at key as a string.
+
+        Args:
+            key (str): Key used to store the data.
+
+        Returns:
+            str: String data.
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Gets data stored at key as an Integer.
+
+        Args:
+            key (str): Key used to store the data.
+
+        Returns:
+            int: Integer data.
+        """
+        return self.get(key, int)
