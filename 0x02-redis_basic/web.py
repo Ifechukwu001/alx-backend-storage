6de6e53c8189152da7b55d8a2165624a1ae9e6db
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-16 16:07:53 +0100

    Refactor the code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,16 +4,37 @@
 """
 import redis
 import requests
+import typing
 
 
-def get_page(url: str) -> str:
+redie = redis.Redis()
 
-    redie = redis.Redis()
-    def wrapper():
+
+def cache(function: typing.Callable) -> typing.Callable:
+    """Caches the time a url is accessed
+
+    Args:
+        function: request function to cache.
+
+    """
+    def wrapper(*args, **kwargs):
+        url = args[0]
         redie.incr("count:{}".format(url))
         redie.expire("count:{}".format(url), 10)
+        return function(*args, **kwargs)
 
-        page = requests.get(url)
-        return page.text
+    return wrapper
 
-    return wrapper()
+
+@cache
+def get_page(url: str) -> str:
+    """Gets an returns a url page
+
+    Args:
+        url (str): String link.
+
+    Returns:
+        Request object.
+    """
+    page = requests.get(url)
+    return page
