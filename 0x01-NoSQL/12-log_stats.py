3049e9fbc871554423f571e686e5862bacfa1150
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-04-16 14:52:47 +0100

    Make script not execute on import

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,22 +6,23 @@ Counts the documents that are in the database
 import pymongo
 
 
-client = pymongo.MongoClient()
-db = client.logs
+if __name__ == "__main__":
+    client = pymongo.MongoClient()
+    db = client.logs
 
-log_count = db.nginx.count_documents({})
-get_count = db.nginx.count_documents({"method": "GET"})
-post_count = db.nginx.count_documents({"method": "POST"})
-put_count = db.nginx.count_documents({"method": "PUT"})
-patch_count = db.nginx.count_documents({"method": "PATCH"})
-delete_count = db.nginx.count_documents({"method": "DELETE"})
-getstats_count = db.nginx.count_documents({"method": "GET", "path": "/status"})
+    log_count = db.nginx.count_documents({})
+    get_count = db.nginx.count_documents({"method": "GET"})
+    post_count = db.nginx.count_documents({"method": "POST"})
+    put_count = db.nginx.count_documents({"method": "PUT"})
+    patch_count = db.nginx.count_documents({"method": "PATCH"})
+    delete_count = db.nginx.count_documents({"method": "DELETE"})
+    stats_cnt = db.nginx.count_documents({"method": "GET", "path": "/status"})
 
-print("{} logs".format(log_count))
-print("Methods:")
-print("    method GET: {}".format(get_count))
-print("    method POST: {}".format(post_count))
-print("    method PUT: {}".format(put_count))
-print("    method PATCH: {}".format(patch_count))
-print("    method DELETE: {}".format(delete_count))
-print("{} status check".format(getstats_count))
+    print("{} logs".format(log_count))
+    print("Methods:")
+    print("    method GET: {}".format(get_count))
+    print("    method POST: {}".format(post_count))
+    print("    method PUT: {}".format(put_count))
+    print("    method PATCH: {}".format(patch_count))
+    print("    method DELETE: {}".format(delete_count))
+    print("{} status check".format(stats_cnt))
